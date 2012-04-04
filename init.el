bfe0a385aadc0a9dcc4a5e2fae514284ea1e9d92
Giovanni Intini <giovanni@mikamai.com>
2012-04-04 12:31:41 +0200

    initial import of new version derived from emacs-starter-kit

---
+++ init.el
@@ -0,0 +1,15 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit starter-kit-bindings starter-kit-eshell starter-kit-js starter-kit-lisp starter-kit-ruby yasnippet-bundle)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
---
+++ intinig.el
@@ -0,0 +1,58 @@
+;; My personal configuration on top of emacs-starter-kit
+
+;; sets the default font to menlo
+(defun fontify-frame (frame)
+  (set-frame-parameter frame 'font "Menlo Regular-14"))
+(fontify-frame nil)
+(push 'fontify-frame after-make-frame-functions)
+
+;; default window size
+(setq default-frame-alist '((width . 105) (height . 38) ))
+
+;; remove trailing whitespaces
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; we go for light color this time
+(load-theme 'adwaita)
+
+;; a bit of slime
+;; I don't like that it references something that I can't be sure
+;; Emacs has by default, but this is currently the best way to have
+;; SLIME work seamlessly.
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/local/bin/ccl") ; your Lisp system
+
+
+;; Apply shell environment to emacs
+(require 'cl)
+(defun env-line-to-cons (env-line)
+  "Convert a string of the form \"VAR=VAL\" to a cons cell containing (\"VAR\" . \"VAL\")."
+  (if (string-match "\\([^=]+\\)=\\(.*\\)" env-line)
+      (cons (match-string 1 env-line) (match-string 2 env-line))))
+
+(defun interactive-env-alist (&optional shell-cmd env-cmd)
+  "Launch /usr/bin/env or the equivalent from a login shell, parsing and returning the
+environment as an alist."
+  (let ((cmd (concat (or shell-cmd "$SHELL -lc")
+                     " "
+                     (or env-cmd "/usr/bin/env"))))
+    (mapcar 'env-line-to-cons
+            (remove-if
+             (lambda (str)
+               (string-equal str ""))
+             (split-string (shell-command-to-string cmd) "[\r\n]")))))
+
+(defun setenv-from-cons (var-val)
+  "Set an environment variable from a cons cell containing
+two strings, where the car is the variable name and cdr is
+the value, e.g. (\"VAR\" . \"VAL\")"
+  (setenv (car var-val) (cdr var-val)))
+
+(defun setenv-from-shell-environment (&optional shell-cmd env-cmd)
+  "Apply the environment reported by `/usr/bin/env' (or env-cmd)
+as launched by `$SHELL -lc' (or shell-cmd) to the current
+environment."
+  (mapc 'setenv-from-cons (interactive-env-alist shell-cmd env-cmd)))
+
+(setenv-from-shell-environment)
+(setq exec-path (split-string (getenv "PATH") path-separator))
